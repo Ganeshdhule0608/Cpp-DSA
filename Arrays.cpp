0a4adc65fafd5c22436b8f2b9ce5a2d90c927963
Ganesh Dhule <143633880+Ganeshdhule0608@users.noreply.github.com>
2024-11-24 21:00:04 +0530

    .

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,23 +1,52 @@
-#include<iostream>
-#include <climits>
-using namespace std;
+// #include<iostream>
+// #include <climits>
+// using namespace std;
 
-int main(){
+// int main(){
 
-    int nums[] = { 5,9,21,-15,24,15};
-    int size = 6;
+//     int nums[] = { 5,9,21,-15,24,15};
+//     int size = 6;
 
-    int biggest = INT_MIN;
+//     int biggest = INT_MIN;
 
-    for(int i = 0; i<size; i++){
-        if(nums[i]>biggest){
-            biggest=nums[i];
-        }
+//     for(int i = 0; i<size; i++){
+//         if(nums[i]>biggest){
+//             biggest=nums[i];
+//         }
 
-    }
+//     }
 
-    cout<<"Smallest number "<<biggest<<endl;
+//     cout<<"Biggest number "<<biggest<<endl;
 
 
     
+// }
+
+
+// Reverse an array
+
+#include<iostream>
+using namespace std;
+
+void reverseArray(int arr[], int size){
+    int start = 0;
+    int end = size-1;
+
+    while(start<end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
+
+int main(){
+
+    int arr[] = {1,2,3,4,5,6,7,8};
+    int size = 8;
+
+    reverseArray(arr,size);
+
+    for(int i = 0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
 }
