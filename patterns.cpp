4308e0ac5a1d5551add73cf8d05233b340227c22
Ganesh Dhule <143633880+Ganeshdhule0608@users.noreply.github.com>
2024-11-24 18:37:50 +0530

    patterns

--- patterns.cpp
+++ patterns.cpp
@@ -2,7 +2,62 @@
 using namespace std;
 
 int main(){
+    // int n;
+    // int num = 1;
+    // cout<<"Enter value of n"<<endl;
+    // cin>>n;
 
-cout<<"hellooo";
-hidaidoiadfoidajfio;
+    // for(int i = 1; i<=n; i++){
+    //     for(int j = 1; j<=n; j++){
+    //         cout<<num<<" ";
+    //         num++;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+    // int n;
+    // cout<<"Enter value of n"<<endl;
+    // cin>>n;
+
+    // for(int i = 1; i<=n; i++){
+    //     for(int j = 1; j<i+1; j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int n;
+    // int num = 1;
+    // cout<<"ENter value of n"<<endl;
+    // cin>>n;
+
+    // for(int i = 1; i<=n; i++){
+    //      for(int j = 1; j<i+1; j++){
+    //         cout<<num<<" ";
+    //                num++;
+    //      }
+        
+  
+    //     cout<<endl;
+
+    // }
+
+
+    int n = 4;
+    int  num = 1;
+
+    for(int i = 0; i<n; i++){
+        for(int j=0; j<i; j++){
+            cout<<" ";
+        }
+        for(int j = 0; j<n-i; j++){
+            cout<<(i+1);
+        }
+        cout<<endl;
+    }
+    
+
 }
+
